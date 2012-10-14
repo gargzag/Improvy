d037da2d97b3453a1e34e5fd8aed3777a4ed10ed
Leo <leo.dlgv@gmail.com>
2012-10-14 10:17:54 +0400

    adsd

---
+++ application/views/main_view.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <div class="row">
+        <div class="span5">
+            <div class="well well-large">
+                <h4>Take your product to new heights.</h4>
+                <p>Bounce is a modern and minimalistic landing page perfect for showing off your latest product or service and its features. Bounce is a wonderful starting point to create something more unique.
+                <p><a href="/courses"><button class="btn btn-large btn-primary" type="button">Совершенствоваться</button></a>
+            </div>
+        </div>   
+        <div class="span7">
+            <div id="myCarousel" class="carousel slide">
+                <!-- Carousel items -->
+                    <div class="carousel-inner">
+                        <div class="active item"><img src="images/fitnes.jpg"/></div>
+                        <div class="item"><img src="images/eng.jpg"/></div>
+                        <div class="item"><img src=""/></div>
+                    </div>
+                <!-- Carousel nav -->
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            
+            </div>
+        </div>
+        
+    </div>
+    <hr />
+    
+    
+    <div class="row">
+        <div class="span4">
+            
+            <img src="images/icon_search.png"/>
+            <span class="text-info text_icon">Курсы на любой вкус</span>
+            <p> Вам больше не придется тратить много времени на поиск интересующих вас курсов или секций, бороздя просторы интернета.
+                
+        </div>
+        <div class="span4">
+            <img src="images/icon_video.png"/>
+            <span class="text-info text_icon">Что это за сервис?</span>
+            <p>Bounce uses free open-source fonts from Fontspring as well as the iconic Font Awesome.
+        </div>
+        <div class="span4">
+            <span class="text-info text_icon"></span>
+            <p>Bounce comes with several different colors, giving you a head start on creating a unique design.
+        </div>
+    </div>
+</div>	
