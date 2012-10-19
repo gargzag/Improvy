49612fae6d787761fa060e9c12c27336be00328d
Leo <leo.dlgv@gmail.com>
2012-10-19 23:03:58 +0400

    up issue

--- application/models/model_courses.php
+++ application/models/model_courses.php
@@ -2,25 +2,32 @@
 
 class Model_Courses extends Model
 {
-	
-	public function get_data()
-	{	
-		//echo "<script type='text/javascript' src='/js/jquery.js'></script>";
+
+
+    public function __construct(){
+
+        //echo "<script type='text/javascript' src='/js/jquery.js'></script>";
         //include '/php/db.php';
-		$localhost  = 'localhost'; // хост
-    	$dbuser     = 'root';      // имя пользователя
-    	$dbpassword = '1q2w3e';          // пароль
-    	$database   = 'improvy';      // база данных
+        $localhost  = 'localhost'; // хост
+        $dbuser     = 'root';      // имя пользователя
+        $dbpassword = '1q2w3e';          // пароль
+        $database   = 'improvy';      // база данных
     
-   		 /*
-    		* Подключение к БД
-    	*/
-    	$db = mysql_pconnect($localhost, $dbuser, $dbpassword) or die('В настоящий момент сервер базы данных не доступен.');  
-    	mysql_query("SET NAMES 'UTF8'");
-    	mysql_select_db($database, $db) or die ('В настоящий момент база данных не доступна.');
+         /*
+            * Подключение к БД
+        */
+        $db = mysql_pconnect($localhost, $dbuser, $dbpassword) or die('В настоящий момент сервер базы данных не доступен.');  
+        mysql_query("SET NAMES 'UTF8'");
+        mysql_select_db($database, $db) or die ('В настоящий момент база данных не доступна.');
+    }
+	
+	public function get_data($course)
+	{	
+        $data = mysql_query("SELECT `$course`.`name_rus` as s, `$course`.`price`, `companies`.`name_rus`
+                            FROM $course JOIN venues on $course.id_venue = venues.id_venue 
+                                         JOIN companies on venues.id_company = companies.id_company");              
+        return $data;
+		
 	}
-    public function languages() {
-    $data = mysql_query("SELECT `languages`.`name`,`price`,`languages`.`description` FROM languages, company, venues WHERE company.id_compnay=1 AND languages.id_venue=venues.id_venue");              
-    return $data;
-    }		
+    	
 	}
