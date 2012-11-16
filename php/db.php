c0455de4dbb9c220a2f696350095acda312a5486
Leo <leo.dlgv@gmail.com>
2012-11-16 18:59:21 +0400

    fd

--- php/db.php
+++ php/db.php
@@ -3,11 +3,11 @@
     $localhost  = 'localhost'; // хост
     $dbuser     = 'root';      // имя пользователя
     $dbpassword = '1q2w3e';          // пароль
-    $database   = 'improvy6';      // база данных
+    $database   = 'improvy';      // база данных
     
     /*
      * Подключение к БД
      */
-    $db = mysql_pconnect($localhost, $dbuser) or die('В настоящий момент сервер базы данных не доступен.');  
+    $db = mysql_pconnect($localhost, $dbuser, $dbpassword) or die('В настоящий момент сервер базы данных не доступен.');  
     mysql_query("SET NAMES 'UTF8'");
     mysql_select_db($database, $db) or die ('В настоящий момент база данных не доступна.');?>
