bfb2ee38730db8945c9311b83e845b595c48df9e
Mikhail <alekseev.miha@gmail.com>
2013-02-20 02:09:18 +0400

    Page Add course v1.0

---
+++ application/controllers/controller_ter.php
@@ -0,0 +1,12 @@
+<?php
+					include "application/models/model_company.php";
+	            	class Controller_ter extends Controller{
+			            function __construct(){
+				            $this->model = new Model_Company();
+				            $this->view = new View();
+				        }
+				        function action_index() {
+				            $data = $this->model->get_data_company();
+				            $this->view->generate("company_view.php","template_view.php",$data);
+			        	}
+		            }
